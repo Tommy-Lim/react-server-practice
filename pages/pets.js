b92fd35370767ece59df8c488bf69121f2963e75
Tommy-Lim <tlim24@gmail.com>
2017-03-17 10:27:22 -0700

    added pets page

---
+++ pages/pets.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PetsList from '../components/pets-list'
+
+export default class PetsListPage{
+  getElements(){
+    return <PetsList/>;
+  }
+
+  getMetaTags() {
+		return [
+			{charset: 'utf8'},
+			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{name: 'description', content: 'hello world, powered by React Server'},
+			{name: 'generator', content: 'React Server'},
+		];
+	}
+}
