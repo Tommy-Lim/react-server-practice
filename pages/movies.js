606e823b33077f7df0713b9203d5f2d7f34fb05d
Tommy-Lim <tlim24@gmail.com>
2017-03-16 23:35:14 -0700

    added axios and movies route

---
+++ pages/movies.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export default class TodosPage{
+  getElements(){
+    return <TodoList/>;
+  }
+
+  getMetaTags() {
+		return [
+			{charset: 'utf8'},
+			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{name: 'description', content: 'hello world, powered by React Server'},
+			{name: 'generator', content: 'React Server'},
+		];
+	}
+}
